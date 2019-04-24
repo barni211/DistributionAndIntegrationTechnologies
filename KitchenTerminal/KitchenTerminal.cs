737bb1bcad441ddd530875dae5cc23d0f2a6fd84
Bartosz Fijałkowski <barni211@gmail.com>
2019-04-24 14:26:01 +0100

    Kitchen terminal is working right now

--- KitchenTerminal/KitchenTerminal.cs
+++ KitchenTerminal/KitchenTerminal.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using BarTenderTerminal;
+using CommonOperations;
+using Model;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,24 +15,49 @@ namespace KitchenTerminal
 {
     public partial class KitchenTerminal : Form
     {
+        private List<Order> _orderList;
         public KitchenTerminal()
         {
             InitializeComponent();
+            SetUpTestData();
+        }
+
+
+        private void SetUpTestData()
+        {
+            List<Dish> dishList = new List<Dish>();
+            dishList.Add(new Dish() { Id = 1, Name = "Dish1", Status = OrderStatus.Sended, Type = OrderDestination.Kitchen });
+            dishList.Add(new Dish() { Id = 2, Name = "Dish2", Status = OrderStatus.Sended, Type = OrderDestination.Kitchen });
+            dishList.Add(new Dish() { Id = 3, Name = "Dish3", Status = OrderStatus.Sended, Type = OrderDestination.Kitchen });
+
+            _orderList = new List<Order>();
+            _orderList.Add(new Order() { Id = "1", Destination = OrderDestination.Kitchen, Status = OrderStatus.Sended, TableNumber = 1, Dishes = dishList });
+            _orderList.Add(new Order() { Id = "2", Destination = OrderDestination.Kitchen, Status = OrderStatus.Sended, TableNumber = 1, Dishes = dishList });
+            _orderList.Add(new Order() { Id = "3", Destination = OrderDestination.Kitchen, Status = OrderStatus.Sended, TableNumber = 1, Dishes = dishList });
+            gvDishCollection.DataSource = _orderList;
         }
 
         private void btnInPreparation_Click(object sender, EventArgs e)
         {
-
+            Order orderToUpdate = GvOperations.GetSelectedOrder(gvDishCollection);
+            GvOperations.UpdateOrder(orderToUpdate, OrderStatus.Preparation, gvDishCollection);
+            ShowDishes dishesForm = new ShowDishes(orderToUpdate);
+            dishesForm.Show();
+            //TODO: Send information to server
         }
 
         private void btnReady_Click(object sender, EventArgs e)
         {
-
+            Order orderToUpdate = GvOperations.GetSelectedOrder(gvDishCollection);
+            GvOperations.UpdateOrder(orderToUpdate, OrderStatus.ReadyToPick, gvDishCollection);
+            //TODO: Send information to server
         }
 
         private void btnPicked_Click(object sender, EventArgs e)
         {
-
+            Order orderToUpdate = GvOperations.GetSelectedOrder(gvDishCollection);
+            GvOperations.UpdateOrder(orderToUpdate, OrderStatus.Picked, gvDishCollection);
+            //TODO: Send information to server
         }
     }
 }
